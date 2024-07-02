ceb2693f4cdf7f2fe90333f3566767dbbeb54b17
sahidDev09 <iamsahidofficial99@gmail.com>
2024-07-03 02:03:16 +0600

    added image in landing page

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,8 +2,8 @@
 
 const Landing = () => {
     return (
-        <div className=" w-full h-screen bg-zinc-900">
-            
+        <div className=" w-full h-screen bg-zinc-900 flex justify-center items-center">
+            <img src="https://i.ibb.co/LCzYb3h/git-Banner-italic-crop.png" alt="" />
         </div>
     );
 };

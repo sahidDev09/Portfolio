9ed6c5a82387544c2c1664cc1f196267996e15e1
sahidDev09 <iamsahidofficial99@gmail.com>
2024-08-31 00:57:08 +0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Footer from "./Components/Shared/Footer";
 import FeaturesProjects from "./Components/FeaturesProjects";
 
 const App = () => {
+  
   const locomotiveScroll = new LocomotiveScroll();
 
   return (
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,7 +1,9 @@
 const Skills = () => {
   return (
     <div className=" p-5">
-      <h1 className=" font-neue text-2xl md:text-4xl text-center">Modern Skills</h1>
+      <h1 className=" font-neue text-2xl md:text-4xl text-center">
+        Modern Skills
+      </h1>
       <div className="flex flex-wrap justify-center gap-2 my-5">
         <h1 className="transition-all hover:scale-105 md:text-xl text-md rounded-full px-4 py-2 bg-zinc-800 hover:bg-[#E04D1D]">
           Html
